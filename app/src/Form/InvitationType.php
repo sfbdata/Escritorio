d36955d85ed528db6af1fdbf30bc68f35036a80f
Samuel <sfb.samuell@gmail.com>
2026-02-15 16:30:44 -0300

    Criação e ajustes dos formulários (FormTypes)

---
+++ app/src/Form/InvitationType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InvitationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fullName', TextType::class, [
+                'label' => 'Nome completo',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Perfil',
+                'choices' => [
+                    'Advogado' => 'ROLE_ADVOGADO',
+                    'Financeiro' => 'ROLE_FINANCEIRO',
+                    'Administrativo' => 'ROLE_ADMINISTRATIVO',
+                ],
+                'expanded' => false,
+                'multiple' => true, // 🔹 agora retorna array
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
---
+++ app/src/Form/TenantType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tenant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class TenantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nome do Tenant',
+            ])
+            ->add('adminEmail', EmailType::class, [
+                'mapped' => false, // não pertence ao Tenant, mas usado para criar User
+                'label' => 'E-mail do Administrador',
+            ])
+            ->add('adminPassword', PasswordType::class, [
+                'mapped' => false,
+                'label' => 'Senha do Administrador',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tenant::class,
+        ]);
+    }
+}
